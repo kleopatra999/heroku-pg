b7a2c80b25f5f29ad2860b9a367d930d006c7a2b
Jeff Dickey <dickeyxxx@gmail.com>
2016-10-08 00:30:51 -0700

    added pg:backups:cancel

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ exports.topics = [
 
 exports.commands = flatten([
   require('./commands/backups'),
+  require('./commands/backups/cancel'),
   require('./commands/info'),
   require('./commands/kill'),
   require('./commands/killall'),
