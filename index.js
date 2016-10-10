bf70f61c82dd50125b9d75e6cbb360d9054a299a
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2016-10-10 15:45:36 -0700

    added pg:copy

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ exports.topics = [
 
 exports.commands = flatten([
   require('./commands/backups'),
+  require('./commands/copy'),
   require('./commands/info'),
   require('./commands/kill'),
   require('./commands/killall'),
